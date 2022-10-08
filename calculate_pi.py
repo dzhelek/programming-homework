6f4f9c45e75bdd4a1c1223e743db1e8b0d71d382
dzhelek <yoan.dzhelekarski@gmail.com>
2022-10-08 17:40:37 +0300

    calculate pi with given precision (calculate_pi.py)

---
+++ calculate_pi.py
@@ -0,0 +1,20 @@
+def pi(n):
+    n = n * 2 + 1
+    denominator = 6
+    while n != 1:
+        denominator = 6 + n**2/denominator 
+        n -= 2
+    return 3 + 1/denominator
+
+if __name__ == '__main__':
+    print(pi(0)) # 1 digit
+    print(pi(2)) # 2 digits
+    print(pi(10)) # 3 digits
+    print(pi(50)) # 5 digits
+    print(pi(100)) # 6 digits
+    print(pi(500)) # 8 digits
+    print(pi(1000)) # 9 digits
+    print(pi(5000)) # 10 digits
+    print(pi(50000)) # 14 digits
+    print(pi(100000)) # 15 digits
+    print(pi(1000000)) # max digits for python float
